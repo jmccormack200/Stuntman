28fa9cde36bca8ebeab86fa43af7c692008945af
Jesse Hoyt <jesselhoyt@gmail.com>
2016-01-02 02:57:29 -0500

    Added Button GeneratorScript

--- Assets/Scripts/SceneSelection/LoadOnClick.cs
+++ Assets/Scripts/SceneSelection/LoadOnClick.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//https://unity3d.com/learn/tutorials/modules/beginner/live-training-archive/creating-a-scene-menu
+
 public class LoadOnClick : MonoBehaviour
 {
 
     public GameObject loadingImage;
     
-    public void LoadScene(int level)
+    public void LoadScene(string level)
     {
         //loadingImage.SetActive(true);
         //Application.LoadLevel(level);
