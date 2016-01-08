fee0af7cac1c5d7476e2dfb70a03a27a957d9189
swoonzj <swoonzj@verizon.net>
2016-01-07 21:49:05 -0500

    Added crew jobs to the Constants class. Moved location of illness text files.

--- Assets/Scripts/Chawactews!/Crew.cs
+++ Assets/Scripts/Chawactews!/Crew.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// A member of the crew. Child of Character class.
+/// </summary>
 public class Crew : Character
 {
-	public int skill { get; private set; }
+    public int skill { get; private set; }
+    public Constants.crewJob job { get; private set; }
 
-	public Crew(string name, int skill) : base(name)
-	{
-		this.skill = skill;
-	}
+    public Crew(string name, int skill, Constants.crewJob job) : base(name)
+    {
+        this.skill = skill;
+        this.job = job;
+    }
 }
