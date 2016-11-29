33c4170a790dce7f2504848f33a90b2a504d9905
swoonzj <swoonzj@verizon.net>
2016-11-29 16:06:49 -0500

    Added basic Power selection button to Pool
    Proof-of-concept for what will ultimately be a power bar with a selector
    which moves back and forth rapidly, a la every golf game ever.
    
    Currently, a text box counts up/down to/from 100. Pressing the button
    will stop the number.
    
    Side note: music is still great

---
+++ Assets/Scripts/Gameplay/PowerSlider.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PowerSlider : MonoBehaviour {
+
+    public int startingValue = 0;
+    public int speed = 1;
+    public int power = 0 ;
+    private bool forward = true; // If true, value counts up. Counts down if false.
+   
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+
+        // If power goes out of bounds, reverse direction
+        if (power < 0 || power > 100)
+            forward = !forward;
+
+        if (forward) power += speed;
+        else power -= speed;
+
+        // For debug: output power to Text
+        GetComponent<Text>().text = power.ToString();
+    }
+
+    public void Stop()
+    {
+        speed = 0;
+    }
+}
